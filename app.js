1913621a399e6b2bfe95a111fd4c09fb3450c1e1
Pouya gh <pouyaghani1@gmail.com>
2021-10-06 22:11:18 +0330

    made saving mode for data

--- app.js
+++ app.js
@@ -2,6 +2,37 @@ const seats=document.querySelectorAll('#seat');
 const movie=document.querySelector('select');
 const spans=document.querySelectorAll('span');
 let moviePrice=0;
+let id1=-1,id2=-1;
+let seatNum=0;
+
+
+
+localStorage.removeItem( "chairs" );
+
+let chairs=JSON.parse(localStorage.getItem('chairs'));
+if(chairs==null)
+{
+    chairs=new Array(60);
+    chairs.forEach(function(chair){
+        chair=0;
+    });
+}
+console.log(chairs);
+
+if(chairs!=null){
+    seats.forEach(seat =>{
+        id2++;
+        if(chairs[id2]==1){
+            seat[id2]=1;
+            seat.className='selected';
+            seatNum++;
+            spans[0].textContent=seatNum;
+            spans[1].textContent=seatNum*moviePrice+'$';
+        }
+    });
+}
+
+
 function checkOptions(){
     if(movie.value==1){
         moviePrice=30;
@@ -19,22 +50,27 @@ function checkOptions(){
 checkOptions();
 console.log(moviePrice);
 movie.addEventListener('change',checkOptions);
-let seatNum=0;
 
-seats.forEach(seat =>{
+seats.forEach((seat,i) =>{
     if(seat.className=='occupied'){return;}
     seat.addEventListener('click',() =>{
         if(seat.className=='selected'){
+            id1++;
             seat.className='empty'
             seatNum--;
             spans[0].textContent=seatNum;
             spans[1].textContent=seatNum*moviePrice;
+            chairs[i]=0;
+            localStorage.setItem('chairs',JSON.stringify(chairs));
         }
         else{
+            id1++;
             seat.className='selected';
             seatNum++;
             spans[0].textContent=seatNum;
             spans[1].textContent=seatNum*moviePrice+'$';
+            chairs[i]=1;
+            localStorage.setItem('chairs',JSON.stringify(chairs));
         }
     });
 })
